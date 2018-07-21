aa11ae511b356705a831814234d2ee7fa997c658
hkumarm <hemanth.kumarmp@lowes.com>
2018-07-21 14:26:21 +0530

    Initial Commit

---
+++ wikisearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wikisearch
+//
+//  Created by Kumar m p, Hemanth - Hemanth on 7/21/18.
+//  Copyright © 2018 Kumar m p, Hemanth - Hemanth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
